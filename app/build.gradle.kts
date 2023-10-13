08aa3632fab2e6c223e88744c3b8a42d202ddb90
Akhil Balakrishnan <akhil.b@litmus7.com>
2023-10-13 22:43:27 +0530

    Successfully added kmmlibrary as dependency from maven local.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,9 @@ android {
 
 dependencies {
 
+    // kmmlibrary from maven local repository
+    implementation("com.litmus7.kmmlibrary:shared:1.0.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
--- app/src/main/java/com/litmus7/kmmandroid/MainActivity.kt
+++ app/src/main/java/com/litmus7/kmmandroid/MainActivity.kt
@@ -3,18 +3,25 @@ package com.litmus7.kmmandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.litmus7.kmmandroid.ui.theme.KmmAndroidTheme
+import com.litmus7.kmmlibrary.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val greeting = Greeting()
+
         setContent {
             KmmAndroidTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(greeting.greet())
                 }
             }
         }
@@ -31,10 +38,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "$name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
